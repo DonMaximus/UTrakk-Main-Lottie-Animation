0ad726dd47ee2fc7f2331763d694baaaf44891af
DonMaximus <maksym.don@gmail.com>
2022-04-05 22:10:48 -0400

    1_commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm-7'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'stage_7.json' // the path to the animation json
+  });
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm-8'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'stage_8.json' // the path to the animation json
+  });
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm-9'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'stage_9.json' // the path to the animation json
+  });
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm-10'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'stage_10.json' // the path to the animation json
+  });
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm-11'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'stage_11.json' // the path to the animation json
+  });
